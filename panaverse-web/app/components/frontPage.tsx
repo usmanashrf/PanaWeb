da618b3437cf31c6bd60725c49ee4ed50a3c2e6d
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2023-01-25 02:00:11 +0500

    create front page

--- panaverse-web/app/components/frontPage.tsx
+++ panaverse-web/app/components/frontPage.tsx
@@ -1,8 +1,10 @@
+'use client';
 
-import {Box, Flex, Text} from "@chakra-ui/react";
+import { motion } from 'framer-motion';
+import { slideIn, staggerContainer, textVariant } from "../utils/motions";
+import {Box, Flex, Text, Image} from "@chakra-ui/react";
 import Loader from '../utils/modelLoader'
 import dynamic from "next/dynamic";
-
 const LazyModel = dynamic(() => import('../utils/panaModel'), {
   ssr: true,
   loading: () => <Loader/>
@@ -12,23 +14,33 @@ export default function FrontPage(){
     return(
       <Box>
       <Flex > 
+        <Flex mt={["25px","25px","115px"]} ml={["25px","25px","115px"]}>
+      <motion.div
+          variants={textVariant(1.2)}
+          className="flex flex-row justify-center items-center"
+        >
+        <Text mt="5px" ml="20px" fontSize={["20px","35px","50px"]} fontWeight="bold">Certified Web 3.0 
+       </Text>
+        <Text  ml="-10px" color="red" fontSize={["20px","35px","50px"]} fontWeight="bold" align="center"> 
+        &
+        </Text>
+        <Text ml="20px" fontSize={["20px","35px","50px"]}fontWeight="bold">
+        Metaverse Developer</Text>
+        </motion.div>
+        </Flex>
+      <motion.div
+        variants={slideIn('right', 'tween', 0.2, 1)}
+        className="relative w-full md:-mt-[20px] -mt-[12px]"
+      >
+        <Image
+          src="/fithead.png"
+          alt="front_cover"
+          className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+        />
+      </motion.div>
         </Flex>
         {/* <LazyModel/> */}
         </Box>
     )
 }
 
-
-// const images = [
-//   "image1.jpg",
-//   "image2.jpg",
-//   "image3.jpg"
-// ];
-
-// function ImageSlider() {
-//   return (
-//     <Carousel>
-//       {/* {images.map((image, index) => (<img key={index} src={image} alt="slider image" />))} */}
-//     </Carousel>
-//   );
-// }
