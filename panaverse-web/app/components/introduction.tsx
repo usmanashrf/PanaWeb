57bb80dd3d10a7dc11b689c20dbd85207336d397
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2023-01-26 02:14:09 +0500

    madjust text in introduction part

--- panaverse-web/app/components/introduction.tsx
+++ panaverse-web/app/components/introduction.tsx
@@ -11,40 +11,44 @@ const LazyModel = dynamic(() => import('../utils/panaModel'), {
 export default function Intro(){
     return(
         <Box mt="100px" h="absolute" bgColor="grey:300" mb="60px">
-        <Flex>
-            <HStack>
+        <Flex justify="space-between" direction={["column","column", "row"]}>
+            {/* <HStack > */}
             
             {/* <Show above="md"> <LazyModel position="relative"></LazyModel> 
             <Image position="relative" src="/community.png"></Image></Show> */}
             {/* <Show below="md"> */}
-            <VStack>
+            <VStack mt={["30px","-100px","-120px"]} ml={["0px","150px","150px"]}>
             <LazyModel/> 
-            <Image position="relative" src="/community.png"></Image>
+            <Image mt="-15px" position="relative" src="/community.png"></Image>
             </VStack>
             {/* </Show> */}
             
             <motion.nav variants={introVariants} initial="hidden" whileInView="show">
-            <VStack  mt={["0","20px","40px"]} >
-            <Text  fontSize={["40px","35px","45px"]} fontWeight="bold"  align="center">
+            <VStack  mt={["0","20px","40px"]} mr="150px">
+            <Text  fontSize={["40px","35px","45px"]} fontWeight="bold"  maxW="md" >
             The Panaverse Community</Text> 
-            <Text align="center">
+            <Text align={["justify"]} ml={['10px']} maxW={["sm","md","md"]} >
             Web3, 3D Metaverse, AI, IoT, Cloud, and Edge technologies expand the internet as we know it
-            </Text>
-            <Text align="center">
             by introducing novel features and advancements.In this brand-new type of curriculum.
-            </Text>
-            <Text align="center">
-             In which students will learn how to make money and boost exports
-            </Text>
-            <Text align="center">
+            In which students will learn how to make money and boost exports</Text>
+            <Text align={["justify"]} ml={['10px']} maxW={["sm","md","md"]} >
             in the classroom and will begin doing so within six months of the program's beginning.
+            It resembles a cross between a corporate venture and an educational project.
+           
+            </Text>
+            {/* <Text align="right">
+            
+            </Text>
+            <Text align="right">
+             
             </Text>
             <Text align="center">
-            It resembles a cross between a corporate venture and an educational project.
             </Text>
+            <Text align="center">
+            </Text> */}
             </VStack>
             </motion.nav>
-            </HStack>
+            {/* </HStack> */}
         </Flex>
         
         </Box>
