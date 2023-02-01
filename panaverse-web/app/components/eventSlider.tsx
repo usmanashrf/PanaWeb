8d02f139db9415bc87bf59146a955f7dc14c6aba
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2023-02-02 00:41:55 +0500

    remove slider shadow

--- panaverse-web/app/components/eventSlider.tsx
+++ panaverse-web/app/components/eventSlider.tsx
@@ -1,11 +1,15 @@
 import { Box,Text,useColorModeValue } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import ElasticCarousel from "../utils/Elastic";
+import { qtr1Variants } from "../utils/motions";
 
 export default function EventsSlider(){
     return(
-        <Box boxShadow={"dark-lg"} mb="20px">
+        <motion.nav variants={qtr1Variants} initial="hidden" whileInView="show">
+        <Box mb="20px">
              <Text mb="-180px" fontSize={["40px","35px","45px"]} fontWeight="bold" textColor={useColorModeValue("blue.600", "blue.300")} align="center">Recent Events</Text>
             <ElasticCarousel></ElasticCarousel>
         </Box>
+        </motion.nav>
     )
 }
